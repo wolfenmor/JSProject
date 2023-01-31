dabbf4de43a97f3a5055ff7fc4fb630b991ef55e
wolfenmor <wolfenmor@gmail.com>
2023-02-01 00:22:36 +0100

    Add first lesson JS

---
+++ JS_lesson1/main.js
@@ -0,0 +1,51 @@
+let str = "Hello";
+let str1 = "owu";
+let str3 = "ua";
+console.log(`${str} ${str1} com ${str3}`)
+
+let num = 10;
+let num1 = 3;
+let num2 = 100;
+let num4 = -999;
+let num5 = 12;
+let num6 = 2.7;
+let num7 = 4;
+console.log(num % num1)
+console.log(num2 / num)
+console.log(num4);
+console.log(num5 + "3")
+
+const PI = 3.14;
+console.log(PI);
+
+console.log(num6);
+
+console.log(num7 * num7);
+
+let res = 7 > 4;
+console.log(res);
+
+let res1 = 20 === "20";
+console.log(res1);
+
+const firstName = "Max";
+const middleName = "Yuriiovuch";
+const lastName = "Hymcyk";
+console.log(`${firstName} ${middleName} ${lastName}`)
+
+let obj = {
+    Name: "Max",
+    surName: "Hymcyk"
+
+}
+console.log(obj);
+
+let a = 100;
+console.log(typeof a)
+
+let b = "100";
+console.log(typeof b);
+
+let c = true;
+
+console.log(typeof c);
