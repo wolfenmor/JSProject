9794049bdbf597ff0136cf5f5990c49cfd789c01
wolfenmor <wolfenmor@gmail.com>
2023-03-04 16:56:54 +0100

    add JS_lesson10

--- JS_lesson10/main.js
+++ JS_lesson10/main.js
@@ -14,8 +14,8 @@ target.addEventListener("click", function () {
 // при кожному перезавантажені сторінки буде додавати до неї +1
 let count = JSON.parse(localStorage.getItem(`count`));
 
-if (count.value && count.value.length > 0) {
-    count.value = 0;
+if (count && count.length > 0) {
+    count = 0;
 }
 count++;
 
@@ -39,30 +39,29 @@ body.appendChild(div1);
 
 let nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100
 
-]
+];
 let count1 = 0;
 
-let body1 = document.querySelector(`body`);
-let next = document.querySelector(".next");
+let next = document.querySelector(`.next`);
 
-next.addEventListener("click", function () {
-    let div2 = document.createElement("div");
+next.addEventListener("click", function (){
+    let div2 = document.createElement(`div`);
     let nextTen = nums.slice(count1, count1+10);
-    div2.append(nextTen.join(`, `));
-    body1.appendChild(div2);
     count1 += 10;
-});
+    div2.append(nextTen.join(", "));
+    body.appendChild(div2);
 
+});
 let prev = document.querySelector(`.prev`);
 
-prev.addEventListener("click", function () {
-    let div3 = document.createElement("div");
-    let prevTen = nums.slice(count1 - 10, count1);
-    div3.append(prevTen.join(`, `));
-    body1.appendChild(div3);
-    if (count1 - 10 >= 0) {
-        count1 -= 10;
-    } else {
-        count1 = 0;
-    }
+prev.addEventListener("click", function (){
+    let div3 = document.createElement(`div`);
+    let prevTen = nums.slice(count1, count1+10);
+    count1 -= 10;
+    div3.append(prevTen.join(", "));
+    body.appendChild(div3);
 });
+
+//*** Створити 3 інпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
+    // При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
+// (Додатковачастина для завдання)
